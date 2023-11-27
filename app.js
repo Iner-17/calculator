e744183851d882cca49d2e0f22403531a8d67671
Iner-17 <inercollado52@gmail.com>
2023-11-27 09:01:25 +0800

    added history

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const displayField = document.getElementById("field");
+const history = document.querySelector(".history");
 let firstNum = 1;
 let operation;
-let result = 0;
 
 function append(x) {
     displayField.value == 0
@@ -11,62 +11,72 @@ function append(x) {
     if (firstNum == 0) {
         displayField.value = x;
         firstNum = 1;
+        history.innerText = "";
     }
 }
 
-function clearDisplay() {
+const clearDisplay = () => {
     displayField.value = 0;
-}
+    history.innerText = "";
+};
 
-function add() {
+const clearOne = () => {
+    displayField.value == 0
+        ? (displayField.value = 0)
+        : (displayField.value = displayField.value.slice(0, -1));
+};
+
+const add = () => {
     firstNum = displayField.value;
     operation = "+";
     displayField.value = "0";
-    console.log(firstNum);
-}
+};
 
-function subtract() {
+const subtract = () => {
     firstNum = displayField.value;
     operation = "-";
     displayField.value = "0";
-    console.log(firstNum);
-}
+};
 
-function multiply() {
+const multiply = () => {
     firstNum = displayField.value;
     operation = "*";
     displayField.value = "0";
-    console.log(firstNum);
-}
+};
 
-function divide() {
+const divide = () => {
     firstNum = displayField.value;
     operation = "/";
     displayField.value = "0";
-    console.log(firstNum);
-}
+};
 
-function getResult() {
+const getResult = () => {
     switch (operation) {
         case "+":
-            result = firstNum + displayField.value;
             displayField.value =
-                parseInt(firstNum) + parseInt(displayField.value);
+                parseFloat(firstNum) + parseFloat(displayField.value);
+            history.innerText =
+                firstNum + "+" + parseFloat(displayField.value - firstNum);
             break;
 
         case "-":
             displayField.value =
-                parseInt(firstNum) - parseInt(displayField.value);
+                parseFloat(firstNum) - parseFloat(displayField.value);
+            history.innerText =
+                firstNum + "-" + parseFloat(firstNum - displayField.value);
             break;
         case "/":
             displayField.value =
-                parseInt(firstNum) / parseInt(displayField.value);
+                parseFloat(firstNum) / parseFloat(displayField.value);
+            history.innerText =
+                firstNum + "/" + parseFloat(firstNum / displayField.value);
             break;
         case "*":
             displayField.value =
-                parseInt(firstNum) * parseInt(displayField.value);
+                parseFloat(firstNum) * parseFloat(displayField.value);
+            history.innerText =
+                firstNum + "*" + parseFloat(displayField.value / firstNum);
             break;
     }
     firstNum = 0;
-    console.log(firstNum);
-}
+};
