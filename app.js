898c040780164071f81664de98cdab2dc650dfe9
Iner-17 <inercollado52@gmail.com>
2023-11-27 10:36:01 +0800

    added light mode

--- app.js
+++ app.js
@@ -1,7 +1,64 @@
 const displayField = document.getElementById("field");
 const history = document.querySelector(".history");
+const container = document.querySelector(".container");
+const sun = document.getElementById('sun')
+const moon = document.getElementById('moon')
+const nav = document.getElementById('nav')
+const mainText = document.getElementById('main-text')
+const simple = document.getElementById('simple')
+const buttons = document.getElementsByTagName('button')
+
+let mode = document.getElementById('mode')
 let firstNum = 1;
 let operation;
+
+
+moon.addEventListener('click', () => {
+    if (sun.style.display == 'none') {
+        sun.style.display = 'block'; 
+        moon.style.display = 'none';
+        mode.innerText = 'light mode'
+        mode.style.color = "rgb(18 24 27)"
+        nav.style.backgroundColor = '#fff';
+        container.style.backgroundColor = '#fff';
+        mainText.style.color = 'rgb(18 24 27)';
+        simple.style.color = 'rgb(239 68 68)';
+        displayField.style.backgroundColor = 'rgb(234 179 8)';
+        for (const button of buttons) {
+            button.style.backgroundColor = 'rgb(239 68 68)';
+        }
+        
+    }  else {
+        sun.style.display = 'none'
+        moon.style.display = 'block'
+    }
+    
+
+
+})
+
+sun.addEventListener('click', () => {
+
+    if (moon.style.display == 'none') {
+        moon.style.display = 'block'; 
+        sun.style.display = 'none';
+        mode.innerText = 'dark mode'
+        mode.style.color = "#fff"
+        nav.style.backgroundColor = 'rgb(18 24 27)';
+        container.style.backgroundColor = 'rgb(18 24 27)';
+        mainText.style.color = '#fff'
+        simple.style.color = 'rgb(234 179 8)'
+        displayField.style.backgroundColor = 'rgb(239 68 68)';
+        for (const button of buttons) {
+            button.style.backgroundColor = 'rgb(234 179 8)';
+        }
+    }  else {
+        moon.style.display = 'none'
+        sun.style.display = 'block'
+    }
+
+})
+
 
 function append(x) {
     displayField.value == 0
